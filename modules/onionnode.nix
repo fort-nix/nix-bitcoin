14a073c35aece3d9736186f49f5b187332e3fff6
Jonas Nick <jonasd.nick@gmail.com>
2018-11-20 00:22:16 +0000

    Add bitcoind onion service

--- modules/onionnode.nix
+++ modules/onionnode.nix
@@ -18,16 +18,15 @@ in {
   config = mkIf cfg.enable {
     services.bitcoin.enable = true;
     services.bitcoin.listen = true;
-    # make an onion listen node
     services.tor.enable = true;
     services.tor.client.enable = true;
     services.bitcoin.proxy = config.services.tor.client.socksListenAddress;
-    services.tor.hiddenServices =
-      { "bitcoind".map = [ {
-        #port = config.services.bitcoin.port;
-        port = 8333;
-      } ];
-      "bitcoind".version = 3;};
-     #services.tor.hiddenServices.bitcoind.version = 3;
+    services.bitcoin.port = 8333;
+    services.tor.hiddenServices.bitcoind = {
+      map = [{
+        port = config.services.bitcoin.port;
+      }];
+      version = 3;
     };
+  };
 }
