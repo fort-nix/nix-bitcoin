5e40066c7f0cb99ba575e3baf03e53aa1b3b545d
Clemens Fruhwirth <clemens@endorphin.org>
2019-03-25 14:27:21 +0100

    nanopos, lightning-charge and spark-wallet: Package via node2nix
    The strategy of invoking node2nix inside a derivation (installPhase in
    this case) does not work, as under NixOS installations there is no
    network traffic allowed during a derivation build. Hence, we move
    node2nix outside and rewrite the packaging into the modules.
    
    Also switch to callPackage instead of plain imports. This could
    probably be done on all other imported packages inside of
    nix-bitcoin-pkgs.nix.

--- modules/lightning-charge.nix
+++ modules/lightning-charge.nix
@@ -30,7 +30,7 @@ in {
       after = [ "clightning.service" ];
       serviceConfig = {
           EnvironmentFile = "/secrets/lightning-charge-api-token";
-          ExecStart = "${pkgs.lightning-charge.package}/bin/charged -l ${config.services.clightning.dataDir} -d ${config.services.clightning.dataDir}/lightning-charge.db";
+          ExecStart = "${pkgs.lightning-charge}/bin/charged -l ${config.services.clightning.dataDir} -d ${config.services.clightning.dataDir}/lightning-charge.db";
           # Unfortunately c-lightning doesn't allow setting the permissions of the rpc socket,
           # so this must run as the clightning user
           # https://github.com/ElementsProject/lightning/issues/1366
