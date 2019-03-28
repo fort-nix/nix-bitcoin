687bf8017d69096315fef77c068ebbb6c48a0ff8
Clemens Fruhwirth <clemens@endorphin.org>
2019-03-28 09:13:04 +0100

    Make repository importable as NUR (including an overlay)
    https://github.com/nix-community/NUR is a Nix community project that
    aims to make out of tree derivations more easily discoverable and
    accessible to Nix users. Converting the nix-bitcoin repo to conform to
    that style is a minor change and enhances reusability of its
    components. For instance, I could slap on the clightning module more
    easily onto my existing bitcoin node without having to redeploy the
    whole as nixops driven installation. Having the repo in NUR style
    would make that easier.

---
+++ default.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+{
+  # 'lib', 'modules' and 'overlays' are special, see
+  # https://github.com/nix-community/NUR for more.
+  modules = import ./modules; # NixOS modules
+
+  nodeinfo = pkgs.callPackage ./pkgs/nodeinfo { };
+  banlist = pkgs.callPackage ./pkgs/banlist { };
+  lightning-charge = pkgs.callPackage ./pkgs/lightning-charge { };
+  nanopos = pkgs.callPackage ./pkgs/nanopos { };
+  spark-wallet = pkgs.callPackage ./pkgs/spark-wallet { };
+  electrs = pkgs.callPackage ./pkgs/electrs { };
+  liquidd = pkgs.callPackage ./pkgs/liquidd { };
+}
