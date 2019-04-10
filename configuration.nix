51526089a63f9e4f7c2a51093c0a0bcbcb6067ee
nixbitcoin <nixbitcoin@i2pmail.org>
2019-04-10 11:23:10 +0200

    Improve documentation

--- configuration.nix
+++ configuration.nix
@@ -16,17 +16,17 @@
     services.nix-bitcoin.enable = true;
     # Enable this module to use clightning, a Lightning Network implementation in C.
     services.clightning.enable = true;
-    # Enable this module to use lightning-charge, a simple drop-in solution for accepting lightning payments.
+    # Enable this module to use lightning-charge, a simple drop-in solution for accepting lightning payments. Only enable this if clightning is enabled.
     # services.lightning-charge.enable = true;
-    # Enable this module to use nanopos, a simple Lightning point-of-sale system, powered by Lightning Charge. Only enable this if lightning-charge is enabled.
+    # Enable this module to use nanopos, a simple Lightning point-of-sale system, powered by Lightning Charge. Only enable this if clightning and lightning-charge are enabled.
     # services.nanopos.enable = true;
-    # Enable this module to use the nix-bitcoin-webindex, a simple website displaying your node information and link to nanopos store. Only enable this if nanopos is enabled.
+    # Enable this module to use the nix-bitcoin-webindex, a simple website displaying your node information and link to nanopos store. Only enable this if clightning, lightning-charge, and nanopos are enabled.
     # services.nix-bitcoin-webindex.enable = true;
-    # Enable this option to listen for incoming connections. By default nix-bitcoin nodes offer outgoing connectivity. 
+    # Enable this option to listen for incoming lightning connections. By default nix-bitcoin nodes offer outgoing connectivity. 
     # services.clightning.autolisten = true;
     # Enable this module to use liquidd, a daemon for an inter-exchange settlement network linking together cryptocurrency exchanges and institutions around the world.
     # services.liquidd.enable = true;
-    # Enable this module to use spark-wallet, a minimalistic wallet GUI for c-lightning, accessible over the web or through mobile and desktop apps.
+    # Enable this module to use spark-wallet, a minimalistic wallet GUI for c-lightning, accessible over the web or through mobile and desktop apps. Only enable this if clightning is enabled.
     # services.spark-wallet.enable = true;
     # Enable this module to use electrs, an efficient re-implementation of Electrum Server in Rust.
     # services.electrs.enable = true;
