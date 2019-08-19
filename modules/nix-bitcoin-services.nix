7771a4c931f06c75f9f1f27091a67a28cd1cd0a8
Jonas Nick <jonasd.nick@gmail.com>
2019-08-19 20:44:10 +0000

    Refer to systemd man pages for hardening options

--- modules/nix-bitcoin-services.nix
+++ modules/nix-bitcoin-services.nix
@@ -1,3 +1,6 @@
+# See `man systemd.exec` and `man systemd.resource-control` for an explanation
+# of the various systemd options available through this module.
+
 { config, lib, pkgs, ... }:
 
 with lib;
@@ -41,6 +44,3 @@ in
     '';
   };
 }
-
-
-
