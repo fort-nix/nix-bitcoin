26d2e11a6b4913729ad7a4088c36b5c62f5c7ef7
Jonas Nick <jonasd.nick@gmail.com>
2019-10-14 15:06:21 +0000

    Update to NixOS 19.09

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -2,6 +2,6 @@ let
   fetch = rev: builtins.fetchTarball "https://github.com/nixos/nixpkgs-channels/archive/${rev}.tar.gz";
 in
 {
-  nixpkgs = fetch "6420e2649fa9e267481fb78e602022dab9d1dcd1";
-  nixpkgs-unstable = fetch "2436c27541b2f52deea3a4c1691216a02152e729";
+  nixpkgs = fetch "27a5ddcf747fb2bb81ea9c63f63f2eb3eec7a2ec";
+  nixpkgs-unstable = fetch "4cd2cb43fb3a87f48c1e10bb65aee99d8f24cb9d";
 }
