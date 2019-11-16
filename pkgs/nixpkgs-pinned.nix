44372e764fb6c88477b51a9a74351f1d47b789a3
Jonas Nick <jonasd.nick@gmail.com>
2019-11-16 21:47:06 +0000

    Update nixpkgs

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -8,11 +8,11 @@ in
 {
   # To update, run ../helper/fetch-channel REV
   nixpkgs = fetch {
-    rev = "27a5ddcf747fb2bb81ea9c63f63f2eb3eec7a2ec";
-    sha256 = "1bp11q2marsqj3g2prdrghkhmv483ab5pi078d83xkhkk2jh3h81";
+    rev = "07e66484e679d0e28533543f762be20d6d425b66";
+    sha256 = "1d3n1yfp9xhl7nh377sp2wwnh0gscislg6gzj8sgdq169d18lgsg";
   };
   nixpkgs-unstable = fetch {
-    rev = "4cd2cb43fb3a87f48c1e10bb65aee99d8f24cb9d";
-    sha256 = "1d6rmq67kdg5gmk94wx2774qw89nvbhy6g1f2lms3c9ph37hways";
+    rev = "c1966522d7d5fa54db068140d212cba18731dd98";
+    sha256 = "104481nxv0hi1rk3g0fjzyki1668p4b46bz0j3lsqv5gv1nm43vm";
   };
 }
