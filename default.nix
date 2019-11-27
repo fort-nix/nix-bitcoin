7e021a26295e345c6676f598448baaadc9ff33f0
Erik Arvstedt <erik.arvstedt@gmail.com>
2019-11-27 14:04:17 +0100

    simplify overlay.nix
    Move pkg definitions to pkgs/default.nix.
    This allows us to just import the pkgs in overlay.nix and get rid of
    the filtering to exclude the modules.

--- default.nix
+++ default.nix
@@ -1,16 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
-{
-  # 'lib', 'modules' and 'overlays' are special, see
-  # https://github.com/nix-community/NUR for more.
-  modules = import ./modules; # NixOS modules
 
-  nodeinfo = pkgs.callPackage ./pkgs/nodeinfo { };
-  lightning-charge = pkgs.callPackage ./pkgs/lightning-charge { };
-  nanopos = pkgs.callPackage ./pkgs/nanopos { };
-  spark-wallet = pkgs.callPackage ./pkgs/spark-wallet { };
-  electrs = (pkgs.callPackage ./pkgs/electrs { }).rootCrate.build;
-  elementsd = pkgs.callPackage ./pkgs/elementsd { withGui = false; };
-  hwi = pkgs.callPackage ./pkgs/hwi { };
-  pylightning = pkgs.python3Packages.callPackage ./pkgs/pylightning { };
-  liquid-swap = pkgs.python3Packages.callPackage ./pkgs/liquid-swap { };
+(import ./pkgs { inherit pkgs; }) // {
+  modules = import ./modules;
 }
