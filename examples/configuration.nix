0c74c365de8cb5df3b9f8433d5e9baecc8df1aac
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-02-26 17:11:21 +0100

    mention performance loss with hardened kernel profile

--- examples/configuration.nix
+++ examples/configuration.nix
@@ -6,10 +6,11 @@
   imports = [
     <nix-bitcoin/modules/nix-bitcoin.nix>
 
-    # Use hardened kernel profile. See
-    # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/hardened.nix
-    # for the enabled options.
-     <nixpkgs/nixos/modules/profiles/hardened.nix>
+    # FIXME: The hardened kernel profile improves security but
+    # decreases performance by ~50%.
+    # Turn it off when not needed.
+    # Source: https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/hardened.nix
+    <nixpkgs/nixos/modules/profiles/hardened.nix>
 
     # FIXME: Uncomment next line to import your hardware configuration. If so,
     # add the hardware configuration file to the same directory as this file.
