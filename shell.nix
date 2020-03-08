44ccbb91d0a03211f4b6c1a0349db38a21c0ab09
Jonas Nick <jonasd.nick@gmail.com>
2020-03-08 14:14:38 +0000

    Clean up development shell.nix

--- shell.nix
+++ shell.nix
@@ -6,17 +6,7 @@ with import nixpkgs {};
 stdenv.mkDerivation rec {
   name = "nix-bitcoin-environment";
 
-  nixops19_09 = callPackage ./pkgs/nixops {};
-
-  buildInputs = [ nixops19_09 figlet ];
-
   shellHook = ''
-    export NIX_PATH="nixpkgs=${nixpkgs}:."
-    # ssh-agent and nixops don't play well together (see
-    # https://github.com/NixOS/nixops/issues/256). I'm getting `Received disconnect
-    # from 10.1.1.200 port 22:2: Too many authentication failures` if I have a few
-    # keys already added to my ssh-agent.
-    export SSH_AUTH_SOCK=""
-    figlet "nix-bitcoin dev shell"
+    export NIX_PATH="nixpkgs=${nixpkgs}:nix-bitcoin=./:."
   '';
 }
