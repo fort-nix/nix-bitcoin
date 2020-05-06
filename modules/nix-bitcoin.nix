509fca5328a609c75fdea7c086f2db5cc2318c46
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-05-06 12:13:07 +0200

    fix syntax error
    Fixes #172

--- modules/nix-bitcoin.nix
+++ modules/nix-bitcoin.nix
@@ -5,5 +5,5 @@
    imports = [
      ./presets/secure-node.nix
      (lib.mkRemovedOptionModule [ "services" "nix-bitcoin" "enable" ] "Please directly import ./presets/secure-node.nix")
-   ]
+   ];
 }
