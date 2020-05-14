041af87ec13883153a2087f51de8fec32e04208f
Jonas Nick <jonasd.nick@gmail.com>
2020-05-14 22:16:41 +0000

    Update nixpkgs (lnd 0.10, clightning 0.8.2)

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -8,11 +8,11 @@ in
 {
   # To update, run ../helper/fetch-channel REV
   nixpkgs = fetch {
-    rev = "95b9c99f6d091273572ff1ec62b97b6ad3f68bdf";
-    sha256 = "1qcwr9binkwfayix88aljwssxi5djkwscx0rnwlk1yp9q60rgp3d";
+    rev = "7829e5791ba1f6e6dbddbb9b43dda72024dd2bd1";
+    sha256 = "0hs9swpz0kibjc8l3nx4m10kig1fcjiyy35qy2zgzm0a33pj114w";
   };
   nixpkgs-unstable = fetch {
-    rev = "22a3bf9fb9edad917fb6cd1066d58b5e426ee975";
-    sha256 = "089hqg2r2ar5piw9q5z3iv0qbmfjc4rl5wkx9z16aqnlras72zsa";
+    rev = "8ba41a1e14961fe43523f29b8b39acb569b70e72";
+    sha256 = "0c2wn7si8vcx0yqwm92dpry8zqjglj9dfrvmww6ha6ihnjl6mfhh";
   };
 }
