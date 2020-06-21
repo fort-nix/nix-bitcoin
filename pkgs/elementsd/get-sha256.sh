fe6e118bb33ef94eee5c831504a69bd28b6ee8c5
nixbitcoin <nixbitcoin@i2pmail.org>
2020-06-21 10:12:23 +0000

    elementsd: 0.18.1.3 -> 0.18.1.8
    Also includes `get-sha256.sh` to easily determine verified sha256's

---
+++ pkgs/elementsd/get-sha256.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p git gnupg
+set -euo pipefail
+
+TMPDIR="$(mktemp -d -p /tmp)"
+trap "rm -rf $TMPDIR" EXIT
+cd $TMPDIR
+
+echo "Fetching latest release"
+git clone https://github.com/elementsproject/elements 2> /dev/null
+cd elements
+latest=$(git describe --tags `git rev-list --tags --max-count=1`)
+echo "Latest release is ${latest}"
+
+# GPG verification
+export GNUPGHOME=$TMPDIR
+echo "Fetching Steven Roose's Key"
+gpg --keyserver hkps://hkps.pool.sks-keyservers.net --recv-keys DE10E82629A8CAD55B700B972F2A88D7F8D68E87 2> /dev/null
+echo "Verifying latest release"
+git verify-tag ${latest}
+
+echo "tag: ${latest}"
+# The prefix option is necessary because GitHub prefixes the archive contents in this format
+echo "sha256: $(git archive --format tar.gz --prefix=elements-${latest}/ ${latest} | sha256sum | cut -d\  -f1)"
