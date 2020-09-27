84744f38d7ac67c1db98700be2de9e8b8a3fc8ab
Erik Arvstedt <erik.arvstedt@gmail.com>
2020-09-27 12:43:29 +0200

    netns test: disable backup test

--- test/tests.nix
+++ test/tests.nix
@@ -102,6 +102,9 @@ let testEnv = rec {
       imports = [ scenarios.secureNode ];
       nix-bitcoin.netns-isolation.enable = true;
       tests.netns-isolation = true;
+
+      # This test is rather slow and unaffected by netns settings
+      tests.backups = mkForce false;
     };
 
     ## Examples / debug helper
