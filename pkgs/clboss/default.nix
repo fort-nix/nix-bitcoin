9423eadcee379157f639a2c6f4f44fd7b310e164
nixbitcoin <nixbitcoin@i2pmail.org>
2020-12-21 10:50:50 +0000

    clboss: add pkg

---
+++ pkgs/clboss/default.nix
@@ -0,0 +1,26 @@
+{ stdenv, fetchurl, pkgconfig, curl, libev, sqlite }:
+
+let
+  curlWithGnuTLS = curl.override { gnutlsSupport = true; sslSupport = false; };
+in
+stdenv.mkDerivation rec {
+  pname = "clboss";
+  version = "0.10";
+
+  src = fetchurl {
+    url = "https://github.com/ZmnSCPxj/clboss/releases/download/v${version}/clboss-${version}.tar.gz";
+    sha256 = "1bmlpfhsjs046qx2ikln15rj4kal32752zs1s5yjklsq9xwnbciz";
+  };
+
+  nativeBuildInputs = [ pkgconfig libev curlWithGnuTLS sqlite ];
+
+  enableParallelBuilding = true;
+
+  meta = with stdenv.lib; {
+    description = "Automated C-Lightning Node Manager";
+    homepage = "https://github.com/ZmnSCPxj/clboss";
+    maintainers = with maintainers; [ nixbitcoin ];
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+}
