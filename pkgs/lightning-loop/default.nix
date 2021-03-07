6c9c820862516a69ec387a52443f60a279d929f6
nixbitcoin <nixbitcoin@i2pmail.org>
2021-03-07 18:28:08 +0000

    lightning-loop: 0.11.3-beta -> 0.12.0-beta

--- pkgs/lightning-loop/default.nix
+++ pkgs/lightning-loop/default.nix
@@ -2,17 +2,17 @@
 
 buildGoModule rec {
   pname = "lightning-loop";
-  version = "0.11.3-beta";
+  version = "0.12.0-beta";
 
   src = fetchurl {
     url = "https://github.com/lightninglabs/loop/archive/v${version}.tar.gz";
     # Use ./get-sha256.sh to fetch latest (verified) sha256
-    sha256 = "b9eebf39543e7d0cd50ebb29242578146b25781fd5f6ac4c2b7c93448fb65448";
+    sha256 = "95b34bdb54bcf96bda4b2914688244fff8d913fcbc697703b1ec048b39620e23";
   };
 
   subPackages = [ "cmd/loop" "cmd/loopd" ];
 
-  vendorSha256 = "1xml5mbjnp3hs1qmzz98ivjq438l816pphw6iyjjkq44pifnglrz";
+  vendorSha256 = "0r0k6b3pml9silwdzvbvbhgcjk6nf9rp6incyqkwr9kdc2fl0dcw";
 
   meta = with lib; {
     description = " Lightning Loop: A Non-Custodial Off/On Chain Bridge";
