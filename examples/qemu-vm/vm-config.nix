03515a8da600cbe56ba9d4c3c9b33a2cca45ef08
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-03-08 15:11:17 +0100

    examples/qemu: disable hardened preset
    - Hardening is irrelevant in examples
    - The decreased performance is especially noticeable in VMs

--- examples/qemu-vm/vm-config.nix
+++ examples/qemu-vm/vm-config.nix
@@ -1,7 +1,12 @@
 {
-  virtualisation.graphics = false;
-  services.mingetty.autologinUser = "root";
-  users.users.root = {
-    openssh.authorizedKeys.keyFiles = [ ./id-vm.pub ];
+  # Disable the hardened preset to improve VM performance
+  disabledModules = [ <nix-bitcoin/modules/presets/hardened.nix> ];
+
+  config = {
+    virtualisation.graphics = false;
+    services.mingetty.autologinUser = "root";
+    users.users.root = {
+      openssh.authorizedKeys.keyFiles = [ ./id-vm.pub ];
+    };
   };
 }
