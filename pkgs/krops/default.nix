4aa343a82ad2151ff37403e9873f8b1a927679fa
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-03-11 00:44:15 +0100

    fixup! krops pkg: quote url
    unquoted urls are deprecated: https://github.com/NixOS/rfcs/pull/45

--- pkgs/krops/default.nix
+++ pkgs/krops/default.nix
@@ -2,7 +2,7 @@
 
 let
   src = pkgs.fetchgit {
-    url = https://cgit.krebsco.de/krops/;
+    url = "https://cgit.krebsco.de/krops";
     rev = "804c79a14dc8f81a602d31d5a1eed5f82b3f2457";
     sha256 = "1k20l7zqprsrm9s38xslr7190vssf4sjdprd9gh146hxlvln2qrf";
   };
