567dc7f60ef6d7f734b8f110f2a8f90c9f8a6356
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-03-18 19:55:40 +0100

    use nixpkgs-unstable

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -5,14 +5,12 @@ let
       inherit sha256;
     };
 in
-{
+rec {
   # To update, run ../helper/fetch-channel REV
-  nixpkgs = fetch {
-    rev = "6fbc72a353a1b0ae4f5b48cae111bfb1a4d3a529";
-    sha256 = "0aj4xfkwk8gf96ypjp0rcap3hxrqg5qdwgwgx55zk0mlvq9z3h68";
-  };
+  nixpkgs = nixpkgs-unstable;
+
   nixpkgs-unstable = fetch {
-    rev = "c7d0dbe094c988209edac801eb2a0cc21aa498d8";
-    sha256 = "1rwjfjwwaic56n778fvrmv1s1vzw565gqywrpqv72zrrzmavhyrx";
+    rev = "1f77a4c8c74bbe896053994836790aa9bf6dc5ba";
+    sha256 = "1j62nmzz3w33dplzf1xz1pg1pfkxii7lwdqmsxmc71cs9cm3s7n1";
   };
 }
