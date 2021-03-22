d5c53e1510e6c4e8ba6cc136fc99d976d6e2b4ac
nixbitcoin <nixbitcoin@i2pmail.org>
2021-03-22 11:54:28 +0000

    electrs: 0.8.8 -> 0.8.9

--- pkgs/electrs/default.nix
+++ pkgs/electrs/default.nix
@@ -1,19 +1,19 @@
 { lib, rustPlatform, llvmPackages, fetchurl, pkgs }:
 rustPlatform.buildRustPackage rec {
   pname = "electrs";
-  version = "0.8.8";
+  version = "0.8.9";
 
   src = fetchurl {
     url = "https://github.com/romanz/electrs/archive/v${version}.tar.gz";
     # Use ./get-sha256.sh to fetch latest (verified) sha256
-    sha256 = "e6ec3f49c8cb4da27ee6e1ba7323bb6e3af1ebf881719c5710c71d151435660f";
+    sha256 = "e28367127bca9d5444b1061ba7cbf2636788b1db80977da05cd7ba2182dde2ca";
   };
 
   # Needed for librocksdb-sys
   nativeBuildInputs = [ llvmPackages.clang ];
   LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
 
-  cargoSha256 = "0mj2p8810l5jspbsdg3cjz6qk76sczsh9fpifxxsbkznk63qv88j";
+  cargoSha256 = "07bh5mdmi70gy8bmwjzw87d4kc12d5aciiq3slgc07fvhc7nrlmx";
 
   meta = with lib; {
     description = "An efficient Electrum Server in Rust";
