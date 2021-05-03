9588b0af0878d16cc60d58fb1f6359c432ea14c2
Jonas Nick <jonasd.nick@gmail.com>
2021-05-03 18:15:21 +0000

    update nixpkgs
    Includes
    bitcoin: 0.21.0 -> 0.21.1

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -8,11 +8,11 @@ in
 {
   # To update, run ../helper/fetch-channel REV
   nixpkgs = fetch {
-    rev = "5438e11ea34adf2b8111d80e360442077476ff53";
-    sha256 = "14r88yiz69b828hxq1i5xwy63xa4cwzaa88xa4ig05sfsmrf04q1";
+    rev = "dc326c78a93862efb30a76216f527a56496e6284";
+    sha256 = "094zb1p5i5f2nlxny3dc814jvs90nimdj6wwd80495hgs9z76wgp";
   };
   nixpkgs-unstable = fetch {
-    rev = "3d1a7716d7f1fccbd7d30ab3b2ed3db831f43bde";
-    sha256 = "14r8qa6lnzp78c3amzi5r8n11l1kcxcx1gjhnc1kmn4indd43649";
+    rev = "4518794ee53d109d551c210a6d195b79e9995a90";
+    sha256 = "1h86bqrkiydn5nwpndg8k5apdjxff5qigbrrwfam3893vgb7hws2";
   };
 }
