0186b2a76476e07fff7c15aad36a4e4f536948ad
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-09-15 11:32:54 +0200

    examples/shell.nix: Add upgrade note for NixOps users

--- pkgs/generate-secrets-deprecated.nix
+++ pkgs/generate-secrets-deprecated.nix
@@ -11,4 +11,15 @@ throw ''
   else
     cp ${toString ../examples/shell.nix} .
   fi
+
+  Note to NixOps users:
+  - After updating `shell.nix`, secrets are no longer auto-generated
+    when starting the shell. Please manually run shell command `generate-secrets`
+    before deploying.
+  - NixOps version 19.09 that shipped with nix-bitcoin is deprecated and is no longer
+    available in the shell.
+    Instead, you can add a current NixOps version to your shell PATH via `extraShellInitCmds`
+    or migrate to the krops deployment method.
+    See here for a migration guide:
+    ${toString ../docs/nixops-krops-migration.md}
 ''
