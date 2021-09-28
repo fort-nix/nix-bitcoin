16281de0ae0ca78640e3aea7e0a55aeef4dbabeb
yzernik <yzernik@gmail.com>
2021-09-28 00:10:56 -0700

    Add dependencies for squeaknode

--- pkgs/squeaknode/default.nix
+++ pkgs/squeaknode/default.nix
@@ -21,6 +21,9 @@
 , sqlalchemy
 , ecpy
 , cryptography
+, squeakpy
+, typed-config
+, tox
 
 }:
 
@@ -28,15 +31,36 @@ buildPythonPackage rec {
   pname = "squeaknode";
   version = "0.1.156";
 
-  propagatedBuildInputs = [ click ];
+  propagatedBuildInputs = [
+    alembic
+    expiringdict
+    flask
+    flask-cors
+    flask-login
+    flask-wtf
+    googleapis-common-protos
+    grpcio
+    grpcio-tools
+    importlib-resources
+    mypy-protobuf
+    protobuf
+    psycopg2
+    pysocks
+    bitcoinlib
+    pyzmq
+    requests
+    sqlalchemy
+    squeakpy
+    typed-config
+  ];
 
   src = fetchurl {
-    urls = [ "https://github.com/Blockstream/liquid-swap/archive/release_${version}.tar.gz" ];
-    sha256 = "9fa920ee7d03d1af8252131cd7d5a825bb66b8ad536403b4f5437ff6c91a68b1";
+    urls = [ "https://github.com/yzernik/squeaknode/archive/release_${version}.tar.gz" ];
+    sha256 = "TODO";
   };
-  # Not sure if this does anything, but it should
-  installFlags = [ ".[CLI]" ];
-  # The tests unfortunately seem to require the PyQt for the GUI
-  doCheck = false;
+
+  checkInputs = [
+    tox
+  ];
 }
 
