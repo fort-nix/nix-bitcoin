16f5aa0561265bb9af8dc31d4d09d32c3597ad9e
nixbitcoin <nixbitcoin@i2pmail.org>
2021-12-08 12:12:32 +0000

    update to NixOS 21.11

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   '';
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
