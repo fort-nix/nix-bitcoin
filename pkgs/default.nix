59bf0274c0e9fb1d13fc1e7ebb145d9289a43ac0
Erik Arvstedt <erik.arvstedt@gmail.com>
2021-12-20 15:16:38 +0100

    pkgs: add groups, sort alphabetically

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,22 +6,26 @@ in
 , pkgsUnstable ? import nixpkgsPinned.nixpkgs-unstable { config = {}; overlays = []; }
 }:
 let self = {
-  rtl = pkgs.callPackage ./rtl { };
   cl-rest = pkgs.callPackage ./cl-rest { };
-  spark-wallet = pkgs.callPackage ./spark-wallet { };
-  liquid-swap = pkgs.python3Packages.callPackage ./liquid-swap { };
-  joinmarket = pkgs.callPackage ./joinmarket { inherit (self) nbPython3Packages; };
-  generate-secrets = import ./generate-secrets-deprecated.nix;
-  nixops19_09 = pkgs.callPackage ./nixops { };
-  krops = import ./krops { };
-  netns-exec = pkgs.callPackage ./netns-exec { };
-  clightning-plugins = pkgs.recurseIntoAttrs (import ./clightning-plugins pkgs self.nbPython3Packages);
   clboss = pkgs.callPackage ./clboss { };
+  clightning-plugins = pkgs.recurseIntoAttrs (import ./clightning-plugins pkgs self.nbPython3Packages);
+  joinmarket = pkgs.callPackage ./joinmarket { inherit (self) nbPython3Packages; };
+  liquid-swap = pkgs.python3Packages.callPackage ./liquid-swap { };
+  rtl = pkgs.callPackage ./rtl { };
   secp256k1 = pkgs.callPackage ./secp256k1 { };
+  spark-wallet = pkgs.callPackage ./spark-wallet { };
 
   nbPython3Packages = (pkgs.python3.override {
     packageOverrides = import ./python-packages self;
   }).pkgs;
+
+  # Internal pkgs
+  netns-exec = pkgs.callPackage ./netns-exec { };
+  krops = import ./krops { };
+
+  # Deprecated pkgs
+  generate-secrets = import ./generate-secrets-deprecated.nix;
+  nixops19_09 = pkgs.callPackage ./nixops { };
 
   pinned = import ./pinned.nix pkgs pkgsUnstable;
 
