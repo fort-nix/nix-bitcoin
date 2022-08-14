4459feb752f83cbb6e901218c15001aded76d324
Jonas Nick <jonasd.nick@gmail.com>
2022-08-14 14:51:52 +0000

    rtl: set nodejs version to 14

--- pkgs/rtl/default.nix
+++ pkgs/rtl/default.nix
@@ -1,7 +1,14 @@
 { pkgs, lib, makeWrapper }:
 let
   inherit (pkgs) nodejs;
-  nodePackages = import ./composition.nix { inherit pkgs nodejs; };
+  # TODO-EXTERNAL: if we don't set nodejs to version 14 then we get the
+  # following dependency error from npm:
+  #   > npm WARN peer jasmine-core@">=3.8" from karma-jasmine-html-reporter@1.7.0
+  #   > npm WARN node_modules/karma-jasmine-html-reporter
+  #   > npm WARN   dev karma-jasmine-html-reporter@"^1.5.0" from the root project
+  #   > npm ERR! code ENOTCACHED
+  # This error presumably goes away with RTL 0.13.
+  nodePackages = import ./composition.nix { inherit pkgs; nodejs = pkgs."nodejs-14_x"; };
 in
 nodePackages.package.overrideAttrs (old: {
   nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
