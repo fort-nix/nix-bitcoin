5255c7e8bc7f17d3b0bb0524350432a9c27dc8dc
Jonas Nick <jonasd.nick@gmail.com>
2022-09-06 11:45:39 +0000

    pyln-client: remove pkg patch that has become unnecessary

--- pkgs/python-packages/pyln-client/default.nix
+++ pkgs/python-packages/pyln-client/default.nix
@@ -17,11 +17,4 @@ buildPythonPackage rec {
   checkInputs = [ pytestCheckHook ];
 
   postUnpack = "sourceRoot=$sourceRoot/contrib/${pname}";
-
-  # Fix version typo in pyproject.toml
-  # TODO-EXTERNAL:
-  # This is already fixed upstream. Remove this after the next clightning release.
-  postPatch = ''
-    sed -i 's|pyln-bolt7 = "^1.0.186"|pyln-bolt7 = "^1.0.2.186"|' pyproject.toml
-  '';
 }
