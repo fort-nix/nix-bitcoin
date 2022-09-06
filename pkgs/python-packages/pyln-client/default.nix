63f8b740268792d10475bdc8d203f50fa1641583
Jonas Nick <jonasd.nick@gmail.com>
2022-09-06 11:43:46 +0000

    pyln-client: add patch that fixes plugins
    Without this patch, the summary and feeadjuster plugins crash on startup.

--- pkgs/python-packages/pyln-client/default.nix
+++ pkgs/python-packages/pyln-client/default.nix
@@ -16,5 +16,13 @@ buildPythonPackage rec {
 
   checkInputs = [ pytestCheckHook ];
 
+  # TODO-EXTERNAL:
+  # This patch is a variant (fixed relative path) of
+  # https://github.com/ElementsProject/lightning/pull/5574. This is already
+  # fixed upstream. Remove this after the next clightning release.
+  patches = [
+    ./msat-null.patch
+  ];
+
   postUnpack = "sourceRoot=$sourceRoot/contrib/${pname}";
 }
