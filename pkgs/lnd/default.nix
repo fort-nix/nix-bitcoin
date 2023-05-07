1d69c9c824a1c7e3dfda9e98b431023b70e2f3e3
Erik Arvstedt <erik.arvstedt@gmail.com>
2023-05-07 22:23:38 +0200

    lnd: fix non-static patch URL

--- pkgs/lnd/default.nix
+++ pkgs/lnd/default.nix
@@ -5,7 +5,7 @@ lnd.overrideAttrs (_: {
     (fetchpatch {
       # https://github.com/lightningnetwork/lnd/pull/7672
       name = "fix-PKCS8-cert-key-support";
-      url = "https://github.com/lightningnetwork/lnd/pull/7672.patch";
+      url = "https://github.com/lightningnetwork/lnd/commit/bfdd5db0d97a6d65489d980a917bbd2243dfe15c.patch";
       hash = "sha256-j9EirxyNi48DGzLuHcZ36LrFlbJLXrE8L+1TYh5Yznk=";
     })
   ];
