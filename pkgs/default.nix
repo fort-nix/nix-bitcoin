3650d4befe6cad275b259c78f5373f8e851d070f
Jonas Nick <jonasd.nick@gmail.com>
2023-06-01 08:58:16 +0000

    bitcoin: replace nixpkgs package with bitcoin{,d} 24.1
    Bitcoin Core 25.0 may sometimes hang when shutting down.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,6 +10,21 @@ in
   }
 }:
 let self = {
+  # TODO-EXTERNAL:
+  # Remove bitcoin and bitcoind 24.1 packages and replace with 25.0 from nixpkgs
+  # when https://github.com/bitcoin/bitcoin/issues/27722 has been resolved
+  bitcoin  = pkgsUnstable.libsForQt5.callPackage ./bitcoin {
+    stdenv = if pkgsUnstable.stdenv.isDarwin then pkgsUnstable.darwin.apple_sdk_11_0.stdenv else pkgsUnstable.stdenv;
+    boost = pkgsUnstable.boost17x;
+    withGui = true;
+    inherit (pkgsUnstable.darwin) autoSignDarwinBinariesHook;
+  };
+
+  bitcoind = pkgsUnstable.callPackage ./bitcoin {
+    boost = pkgsUnstable.boost17x;
+    withGui = false;
+    inherit (pkgsUnstable.darwin) autoSignDarwinBinariesHook;
+  };
   clightning-rest = pkgs.callPackage ./clightning-rest { inherit (self) fetchNodeModules; };
   clboss = pkgs.callPackage ./clboss { };
   clightning-plugins = pkgs.recurseIntoAttrs (import ./clightning-plugins pkgs self.nbPython3Packages);
