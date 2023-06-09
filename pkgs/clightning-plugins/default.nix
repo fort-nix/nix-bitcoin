1b741c7cc11e61456d8deb9cf150978fec76f2cd
Erik Arvstedt <erik.arvstedt@gmail.com>
2023-06-09 14:40:32 +0200

    clightning-plugins/prometheus: fix patch not being applied
    `patchPhase` can't be used with `buildCommand`.

--- pkgs/clightning-plugins/default.nix
+++ pkgs/clightning-plugins/default.nix
@@ -3,14 +3,25 @@ pkgs: nbPython3Packages:
 let
   inherit (pkgs) lib;
 
-  src = pkgs.fetchFromGitHub {
-    owner = "lightningd";
-    repo = "plugins";
-    rev = "eee5e90df442586b7f891df4fc0c67d273534737";
-    sha256 = "0ry6gxp9gqpzpdjykx0a5q53saai5jydwvcy6smsh0f5dmjl8srh";
+  src = pkgs.applyPatches {
+    src = pkgs.fetchFromGitHub {
+      owner = "lightningd";
+      repo = "plugins";
+      rev = "eee5e90df442586b7f891df4fc0c67d273534737";
+      sha256 = "0ry6gxp9gqpzpdjykx0a5q53saai5jydwvcy6smsh0f5dmjl8srh";
+    };
+
+    patches = [
+      # https://github.com/lightningd/plugins/pull/451
+      (pkgs.fetchpatch {
+        name = "202305-prometheus-msat-purge";
+        url = "https://github.com/lightningd/plugins/commit/f8a27b97a1b9ded8790c1f033b1f4268c0a6e210.patch";
+        sha256 = "sha256-0lFMhHHIi9bUU0+xaHhpnascNlFmr51JxE6e2F0s0zc=";
+      })
+    ];
   };
 
-  version = builtins.substring 0 7 src.rev;
+  version = builtins.substring 0 7 src.src.rev;
 
   plugins = with nbPython3Packages; {
     currencyrate = {
@@ -33,14 +44,6 @@ let
       patchRequirements =
         "--replace prometheus-client==0.6.0 prometheus-client==0.15.0"
         + " --replace pyln-client~=0.9.3 pyln-client~=23.02";
-      patches = [
-        # https://github.com/lightningd/plugins/pull/451
-        (pkgs.fetchpatch {
-          name = "202305-prometheus-msat-purge";
-          url = "https://github.com/lightningd/plugins/commit/f8a27b97a1b9ded8790c1f033b1f4268c0a6e210.patch";
-          sha256 = "sha256-0lFMhHHIi9bUU0+xaHhpnascNlFmr51JxE6e2F0s0zc=";
-        })
-      ];
     };
     rebalance = {
       description = "Keeps your channels balanced";
@@ -81,8 +84,6 @@ let
         chmod +x '${script}'
         patchShebangs '${script}'
       '';
-
-      patches = plugin.patches or [];
 
       passthru.path = "${drv}/${script}";
 
