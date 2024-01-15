73d6d1e8cab384f2f2eff7f13e621aba6324f669
Erik Arvstedt <erik.arvstedt@gmail.com>
2024-01-15 23:04:29 +0100

    dev: move mempool to `dev-features.sh`
    The mempool dev cmd docs are too short for a dedicated file.

--- dev/dev-features.sh
+++ dev/dev-features.sh
@@ -289,5 +289,29 @@ c journalctl -u clightning -f
 # plugin-trustedcoin returning block 801409, 0000000000000000000482ddc4…, 1483968 bytes
 
 #―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+# mempool
+run-tests.sh -s mempool-regtest container
+
+c systemctl status mempool
+c systemctl status mysql
+c nodeinfo
+
+# Check backend
+c curl -fsS localhost:8999/api/v1/blocks/1 | jq
+c curl -fsS localhost:8999/api/v1/blocks/tip/height | jq
+c curl -fsS localhost:8999/api/v1/address/1CGG9qVq2P6F7fo6sZExvNq99Jv2GDpaLE | jq
+
+# Check frontend
+c curl -fsS localhost:60845
+c curl -fsS localhost:60845/api/mempool | jq
+c curl -fsS localhost:60845/api/blocks/1 | jq
+c curl -fsS localhost:60845/api/v1/blocks/1 | jq
+c curl -fsS localhost:60845/api/blocks/tip/height | jq
+
+# Open frontend
+# shellcheck disable=SC2154
+runuser -u "$(logname)" -- xdg-open "http://$ip:60845/"
+
+#―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
 # rtl
 # see ./topics/rtl.sh
