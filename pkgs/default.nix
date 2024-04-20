97eaa5a604be86d30e249dff0fea111809af6be3
Jonas Nick <jonasd.nick@gmail.com>
2024-04-20 07:50:46 +0000

    update nixpkgs
    btcpayserver: 1.12.5 -> 1.13.1
    clightning: 24.02.1 -> 24.02.2
    hwi: 2.4.0 -> 3.0.0
    nbxplorer: 2.5.0 -> 2.5.2
    
    Don't update bitcoin to 27.0 due to compatibility issues with lnd.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,6 +10,22 @@ in
   }
 }:
 let self = {
+  # TODO-EXTERNAL:
+  # Remove bitcoin and bitcoind 26.x packages and replace with 27.0 from nixpkgs
+  # when a version of lnd is released that is compatible with 27.0
+  # (https://github.com/lightningnetwork/lnd/pull/8664).
+  bitcoin = let inherit (pkgsUnstable) libsForQt5 stdenv darwin; in
+    libsForQt5.callPackage ./bitcoin {
+      stdenv = if stdenv.isDarwin then darwin.apple_sdk_11_0.stdenv else stdenv;
+      withGui = true;
+      inherit (darwin) autoSignDarwinBinariesHook;
+  };
+
+  bitcoind = let inherit (pkgsUnstable) callPackage darwin; in
+    callPackage ./bitcoin {
+      withGui = false;
+      inherit (darwin) autoSignDarwinBinariesHook;
+  };
   clightning-rest = pkgs.callPackage ./clightning-rest { inherit (self) fetchNodeModules; };
   clboss = pkgs.callPackage ./clboss { };
   clightning-plugins = pkgs.recurseIntoAttrs (import ./clightning-plugins pkgs self.nbPython3Packages);
