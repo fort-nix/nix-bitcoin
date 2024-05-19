946a0b844145a79ccc39f6cc3958669ea915f7a6
Otto Sabart <seberm@seberm.com>
2024-05-19 21:00:00 +0200

    python-bitcointx: 1.1.4 -> 1.1.5

--- pkgs/python-packages/python-bitcointx/get-sha256.sh
+++ pkgs/python-packages/python-bitcointx/get-sha256.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
-. "${BASH_SOURCE[0]%/*}/../../../helper/run-in-nix-env" "git gnupg" "$@"
+. "${BASH_SOURCE[0]%/*}/../../../helper/run-in-nix-env" "git gnupg jq" "$@"
 
-TMPDIR=$(mktemp -d -p /tmp)
-trap 'rm -rf $TMPDIR' EXIT
-cd "$TMPDIR"
+latest=$(curl -s "https://api.github.com/repos/Simplexum/python-bitcointx/tags" | jq -r '.[0].name')
+echo "Latest release is $latest"
 
-echo "Fetching latest release"
-git clone https://github.com/simplexum/python-bitcointx 2> /dev/null
-cd python-bitcointx
-latest=python-bitcointx-v1.1.4
-echo "Latest release is ${latest}"
+tmpdir=$(mktemp -d /tmp/python-bitcointx-verify-gpg.XXX)
+trap 'rm -rf $tmpdir' EXIT
+repo=$tmpdir/repo
+git clone --depth 1 --branch "$latest" -c advice.detachedHead=false https://github.com/Simplexum/python-bitcointx "$repo"
 
 # GPG verification
-export GNUPGHOME=$TMPDIR
+export GNUPGHOME=$tmpdir
 echo "Fetching Dimitry Pethukov's Key"
 gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys B17A35BBA187395784E2A6B32301D26BDC15160D 2> /dev/null
-echo "Verifying latest release"
-git verify-commit "$latest"
+echo
+echo "Verifying commit"
+git -C "$repo" checkout -q "tags/$latest"
+git -C "$repo" verify-commit HEAD
+rm -rf "$repo"/.git
+hash=$(nix hash path "$repo")
 
-git checkout -q "tags/$latest"
-rm -rf .git
-
+echo
 echo "tag: $latest"
-nix hash path .
+echo "hash: $hash"
