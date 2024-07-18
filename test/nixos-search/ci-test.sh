3cc60106583a20a0825ea3f8823f2f694f73b99e
Erik Arvstedt <erik.arvstedt@gmail.com>
2024-07-18 20:45:36 +0200

    test/nixos-search/ci-test.sh: remove unneded setup step
    Sandbox is off by default in the `docker-nixpkgs` image.

--- test/nixos-search/ci-test.sh
+++ test/nixos-search/ci-test.sh
@@ -8,10 +8,6 @@ cachixCache=nix-bitcoin
 
 nix run .#cachix -- use "$cachixCache"
 
-# We're running in a basic, unprivileged container that doesn't support sandboxing.
-# Sandboxing is unnneeded because we're only building the 3rd-party `flake-info` tool.
-echo "sandbox = false" >> /etc/nix/nix.conf
-
 # shellcheck disable=SC2016
 PATH=$(nix shell -L .#flake-info .#cachix -c sh -c 'echo $PATH')
 
